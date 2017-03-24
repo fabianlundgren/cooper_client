247072d21e359e3209693aab1b9e442363a3f3be
fabianlundgren <fabbe.lundgren@gmail.com>
2017-03-24 20:32:52 +0100

    Fixed issue with bmi calculator

--- www/js/person.js
+++ www/js/person.js
@@ -1,16 +1,13 @@
 function Person(attr) {
   this.gender = attr.gender;
   this.age = attr.age;
+  this.weight = attr.weight;
+  this.height = attr.height;
 }
 
 Person.prototype.assessCooper = function (distance) {
   this.cooperMessage = cooperAssessmentOf(this, distance);
 };
-
-function Person(attr) {
-  this.weight = attr.weight;
-  this.height = attr.height;
-}
 
 Person.prototype.calculate_bmi_met = function() {
   calculator = new BMICalculator();
